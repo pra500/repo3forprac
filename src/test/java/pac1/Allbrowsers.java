7fbd8091687db8d2cc7a02e9db15cdc874a8768e
pra500 <pragtig6@gmail.com>
2020-07-31 18:42:28 +0530

    new

---
+++ src/test/java/pac1/Allbrowsers.java
@@ -0,0 +1,60 @@
+package pac1;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class Allbrowsers {
+
+	private static WebDriver driver;
+	
+	
+	
+	@Test
+	@Parameters("brow")
+	public void getbroser(String br)
+	{
+		if(br.equalsIgnoreCase("Chrome"))
+		{
+						
+			System.setProperty("webdriver.chrome.driver","C:\\Users\\Admin\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		    driver= new ChromeDriver();	
+			
+		}
+//		else if(br.equalsIgnoreCase("Firefox"))
+//		{
+//			System.setProperty("webdriver.gecko.driver","C:\\Users\\Admin\\Downloads\\geckodriver-v0.23.0-win64\\geckodriver.exe");
+//		    driver=new FirefoxDriver();
+//		}
+		else if(br.equalsIgnoreCase("IE"))
+		{
+			
+			System.setProperty("webdriver.ie.driver","C:\\Users\\Admin\\Downloads\\IEDriverServer_x64_3.150.1\\IEDriverServer.exe");
+			driver=new InternetExplorerDriver();
+		}
+
+		
+		
+		
+		driver.manage().timeouts().implicitlyWait(12, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.softwaretestingmaterial.com");
+		System.out.println(driver.getTitle());
+		driver.quit();
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
